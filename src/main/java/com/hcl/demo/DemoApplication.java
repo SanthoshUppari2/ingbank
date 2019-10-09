a14256984da42fec83c927384720fec5fcfa8504
IngTraining <joydeep.j@hcl.com>
2019-10-09 10:42:47 +0530

    test

--- src/main/java/com/hcl/demo/DemoApplication.java
+++ src/main/java/com/hcl/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	//comment added by fea1
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
